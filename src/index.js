f1170ba916dbd253da65bfc6c1f2471d2a361108
Ian Wright <wright.ianb@gmail.com>
2019-05-09 12:54:07 -0600

    arrrowowow

--- src/index.js
+++ src/index.js
@@ -36,7 +36,24 @@ function drawNode(node) {
 function drawPointer() {
   return `
     <svg class="pointer" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
-      <line x1="100%" y1="0%" x2="0%" y2="100%" stroke="black" />
+      <defs>
+        <marker
+          id="arrow"
+          viewBox="0 0 10 10"
+          refX="9" refY="5"
+          markerWidth="10"
+          markerHeight="6"
+          orient="auto-start-reverse"
+        >
+          <path d="M 0 0 L 10 5 L 0 10 z" />
+        </marker>
+      </defs>
+      <line
+        x1="100%" y1="0%" x2="0%" y2="95%"
+        stroke="black"
+        stroke-width="2"
+        marker-end="url(#arrow)"
+      />
     </svg>
   `;
 }
